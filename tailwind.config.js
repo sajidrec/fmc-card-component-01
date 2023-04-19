f68475fe72cb84779b1a3efbf91a073d951dc3ca
MD. Sajid Hossain <sajidrec@gmail.com>
2023-04-20 02:48:24 +0600

    2.0.0

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        "desktop": "376px"
+      },
+      colors: {
+        "custom-Dark-cyan": "hsl(158, 36%, 37%)",
+        "custom-Cream": "hsl(30, 38%, 92%)",
+        "custom-Very-dark-blue": "hsl(212, 21%, 14%)",
+        "custom-Dark-grayish-blue": "hsl(228, 12%, 48%)",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat","sans-serif"],
+        Fraunces: ["Fraunces", "serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
